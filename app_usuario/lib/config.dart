37b25c17ede890ff4605ca375dae25578b73b375
sebastian-condori <sebastian-condori@users.noreply.github.com>
2026-08-24 11:38:46 -0400

    fix: actualizar IP LAN de las apps a 192.168.103.177

--- app_usuario/lib/config.dart
+++ app_usuario/lib/config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   static const String apiUrl = String.fromEnvironment(
     'API_URL',
-    defaultValue: 'http://192.168.100.7/transporte_api',
+    defaultValue: 'http://192.168.103.177/transporte_api',
   );
 
   static const Duration timeout = Duration(seconds: 20);
